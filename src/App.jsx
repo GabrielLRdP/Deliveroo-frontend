e4f7ed5872a08303c9f7612ec60768534b38633b
Gabriel <gabriel.le.roy06@gmail.com>
2024-03-25 15:16:48 +0100

    added a simple axios request

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,18 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "./index.css";
+import axios from "axios";
 
 function App() {
-  return <>Salut</>;
+  const [response, setResponse] = useEffect();
+
+  const fetchData = async () => {
+    setResponse(
+      await axios.get("https://site--deliveroo-backend--n5fkvp4ymxn4.code.run/")
+    );
+
+    fetchData();
+  };
+  return <>{response ? response : "chargement"}</>;
 }
 
 export default App;
